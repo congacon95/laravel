c6482973715a2c4b4795a4e98e5b2c9dd3c0a484
phuong <congacon13051995@gmail.com>
2017-10-23 17:08:14 +0700

    add user profile model

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -5,10 +5,14 @@
       <div class="col-lg-4">
         <div class="panel panel-default">
           <div class="panel-heading">
-            {{$user->name}}'s profile.
+            <p class="text-center">
+              {{$user->name}}'s profile.
+            </p>
           </div>
           <div class="panel-body">
-            <img src="{{ Storage::url($user->avatar) }}" with="50px" heght='50px' style='border-radius: 50%;' alt="avatar" >
+            <center>
+              <img src="{{ Storage::url($user->avatar) }}" alt='avatar'>
+            </center>
           </div>
         </div> 
       </div>
